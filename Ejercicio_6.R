b84f63b709d7db38b55257bf38305a088ad36a78
Cande <cande.russof2@gmail.com>
2023-10-03 17:03:56 -0300

    P3

---
+++ Ejercicio_6.R
@@ -0,0 +1,43 @@
+#----------------- Ejercicio 6 -----------------
+rm(list=ls())
+setwd("/home/clinux01/Escritorio/Cande/Practica_3/")
+
+#A partir del archivo ???temp bsas.Rdata??? con datos de temperatura mensual para 
+#diferentes niveles durante el per´??odo 2000-2005 sobre la provincia de Buenos
+#Aires. (Cargar el archivo utilizando la funcion ???load()???)
+
+load("/home/clinux01/Escritorio/Cande/Practica_3/temp_bsas.RData")
+
+#----------------- Inciso a -----------------
+#Obtener el promedio mensual de temperatura en el per??odo analizado para cada 
+#nivel y cada punto de ret??cula. Hagalo por un lado utilizando ciclos y por
+#otro reacomodando el arreglo y utilizando la funcion ???apply()???
+
+dim(variable)
+class(variable)
+#es una array de 4 dimensiones
+nlon<-8
+nlat<-14
+niso<-4
+ntiempo<-72
+
+#Utilizando la funcion apply
+#Dimensiones:
+#Primera dimensión: LONGITUDES
+#Segunda dimensión: LATITUDES
+#Tercera dimensión: NIVELES
+#Cuarta dimensión: TIEMPO
+
+prom_mensual<-apply(variable,c(1,2,3),mean)  
+prom_mensual #al mostrarlo por pantalla me da los promedios mensuales para cada 
+#lat y long de cada uno de los 4 niveles
+dim(prom_mensual)
+
+dim_reticula<-nlat*nlon
+
+reticula<-array(data=NA,dim=c(nlon*nlat,niso))
+latitud<-prom_mensual[1:14]
+longitud<-prom_mensual[1:8]
+reticula[longitud,latitud]<-longitud*latitud
+
+
