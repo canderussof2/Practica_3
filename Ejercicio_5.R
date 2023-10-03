5a646aef55609f342ca53a4b5d0181211ec0d5b8
Cande <cande.russof2@gmail.com>
2023-10-03 12:58:46 -0300

    Practica 3

--- Ejercicio_5.R
+++ Ejercicio_5.R
@@ -5,21 +5,19 @@
 #R que permita ingresar la serie de datos por pantalla, la guarde en un vector y muestre por
 #pantalla la hora en la que ocurrio el maximo valor medido y el valor de la temperatura maxima
 #medida.
-getwd()
-setwd("/Users/cande/Desktop/Labo/Practica_3/")
+#getwd()
+#setwd("/Users/cande/Desktop/Labo/Practica_3/")
 #setwd("/home/clinux01/Escritorio/Cande /Practica_3/")
 rm(list=ls())
 
 temperatura<-c()
 horas<-seq(0,21,by=3)
-
 for (temp in horas){
   t<-as.numeric(readline("Ingrese la temperatura: "))
   while (is.na(t)){
     t<-as.numeric(readline("Ingrese nuevamente la temperatura: "))
   } 
   temperatura<-c(temperatura,t)
-  suma<-temperatura+t
 }
 max<-max(temperatura) #Valor maximo de temperatura
 posicmax<-which(temperatura==max) #posicion donde se da la maxima temp
@@ -33,14 +31,23 @@ print(paste("La mayor temperatura registrada fue de",tempmax,"°C y ocurrió a l
 #Calcular la suma de todos los elementos de la serie de temperaturas y comparar los resultados
 #utilizando la func intrınseca “sum”. Idem con el promedio de las temperaturas 
 #(verificar utilizando la funcion intrınseca “mean”).
+rm(list=ls())
 
-
-for (valor in temperatura) {
-  if(valor>=9) {
-    break
-  } else {
-   suma<-temperatura[valor]+temperatura[valor+1]
+temperatura<-c()
+horas<-seq(0,21,by=3)
+suma<-0
+for (temp in horas){
+  t<-as.numeric(readline("Ingrese la temperatura: "))
+  while (is.na(t)){
+    t<-as.numeric(readline("Ingrese nuevamente la temperatura: "))
   } 
+  temperatura<-c(temperatura,t)
+  suma<-suma+t
 }
-sum<-sum(temperatura)
+max<-max(temperatura) #Valor maximo de temperatura
+posicmax<-which(temperatura==max) #posicion donde se da la maxima temp
+tempmax<-temperatura[which(temperatura==max)] #Valor maximo de temperatura
+horamax<-horas[which.max(temperatura)] 
+print(paste("La mayor temperatura registrada fue de",tempmax,"°C y ocurrió a las",horamax,"horas"))
 
+sum(temperatura)
